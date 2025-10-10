c1c838679bbe11614a852024219d461ade3192e9
Umar <umarakhtar234@outlook.com>
2025-10-10 12:56:06 +0100

    lolo

--- app.py
+++ app.py
@@ -10,17 +10,35 @@ from html2image import Html2Image
 
 app = Flask(__name__) #creates an application in flask
 
+def preview():
+    
+    page_html = requests.get(request.args['url']).text
+
+    parser = etree.HTMLParser()
+    tree = etree.parse(io.StringIO(page_html), parser)
+
+    head = tree.xpath('/html/head')[0]
+    title = head.xpath('meta[@property="og:title"]/@content')[0]
+    description = head.xpath('meta[@property="og:description"]/@content')[0]
+
+    preview_html = render_template('card.html', title=title, excerpt=description)
+
+    hti = Html2Image()
+    hti.screenshot(html_str=preview_html, save_as='preview.png')
+
+    with open('preview.png', 'rb') as f:
+        preview_img = f.read()
+
+    return Response(preview_img, mimetype='image/png')
+
 @app.route("/") # this is the default route for the webpage
-
-
-
 def homepage():
     return render_template("index.html", show_element="none", show_results=" ") # this returns the HTML file so it is presented to the user
+    #return render_template("details.html")
 
 
 
 @app.route("/search", methods=["GET", "POST"]) # this function will receive the form data
-
 def searchCars():
 
     models = []
@@ -48,8 +66,8 @@ def searchCars():
     
                 
                 if (row[0] == make or make == "N") and (row[1] == model or model == "N") and (item == date or date == "N") and (row[3] == colour or colour == "N") and (row[4] == location or location == "N"):
-                    models.append(row[0])
-                    makes.append(row[1])
+                    makes.append(row[0])
+                    models.append(row[1])
                     dates.append(item)
                     colours.append(row[3])
                     locations.append(row[4])
@@ -60,26 +78,21 @@ def searchCars():
         else:
             return render_template("index.html", show_element="block", show_results=" ")
         
-def preview():
-    
-    page_html = requests.get(request.args['url']).text
+@app.route("/details/<detail>", methods=["GET", "POST"])
+def showDetails(detail):
+    result = detail.split("-")
+    new = []
+    for res in result:
+        print(res)
+        res = res.replace("&", " ")
+        print(res)
+        
+        new.append(res)
+        
+        
+    return render_template("details.html", results=new)
+        
 
-    parser = etree.HTMLParser()
-    tree = etree.parse(io.StringIO(page_html), parser)
-
-    head = tree.xpath('/html/head')[0]
-    title = head.xpath('meta[@property="og:title"]/@content')[0]
-    description = head.xpath('meta[@property="og:description"]/@content')[0]
-
-    preview_html = render_template('card.html', title=title, excerpt=description)
-
-    hti = Html2Image()
-    hti.screenshot(html_str=preview_html, save_as='preview.png')
-
-    with open('preview.png', 'rb') as f:
-        preview_img = f.read()
-
-    return Response(preview_img, mimetype='image/png')
 
 
 if __name__ == "__main__":
